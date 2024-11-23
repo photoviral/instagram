8b998cd38bfbeab8a8c2504ee03448580eb93505
photoviral <olomichel9@gmail.com>
2024-11-23 15:28:21 +1100

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('loginForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-    // Ajoutez ici la logique pour vérifier les identifiants
-    alert('Connexion en cours...');
-});
